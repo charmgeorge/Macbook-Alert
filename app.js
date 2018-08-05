e968bc026d6473f269d4ccbbbba6ffbedc23b4a5
Bethany Armitage <bethgrace5@gmail.com>
2018-08-05 12:44:07 -0700

    #3 Adds favicon, adds default route, fixes html tag

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 weatherApp.config(function ($routeProvider) {
 
   $routeProvider
-
   .when('/', {
     templateUrl: 'pages/home.html',
     controller:'homeController'
   })
-
   .when('/forecast', {
     templateUrl: 'pages/forecast.html',
     controller:'forecastController'
-  });
+  })
+  .otherwise({redirectTo:'/'});
 });
 
 
