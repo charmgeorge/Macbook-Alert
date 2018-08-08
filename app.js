5c3a1cc274efe11e0cc4a0f879d5c1989724a6d7
charmgeorge <charmcgeorge@yahoo.com>
2018-08-07 20:23:02 -0700

    refactor api key w variable

--- app.js
+++ app.js
@@ -38,12 +38,14 @@ weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService
 
   $scope.city = cityService.city;
 
-  var url= "https://api.openweathermap.org/data/2.5/weather"
+  var url= "https://api.openweathermap.org/data/2.5/weather";
+  var myKey = config.MY_KEY;
+
   $scope.weatherAPI = $resource(url, {
     callback: "JSON_CALLBACK" }, {get: { method: "JSONP"}});
 
 
-    $scope.weatherResult = $scope.weatherAPI.get({ q:"London,uk", appid: "04feb4b79b1eb18d5094df27d96f1f62"}).$promise.then(function(result){
+    $scope.weatherResult = $scope.weatherAPI.get({ q:"London,uk", appid: myKey}).$promise.then(function(result){
     console.log(result);
   });
 
