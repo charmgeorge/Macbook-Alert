bd74ca0de13f8d9665ad2448ecb5388e0301014c
charmgeorge <charmcgeorge@yahoo.com>
2018-08-22 20:00:11 -0700

    added temp to display

--- app.js
+++ app.js
@@ -37,18 +37,23 @@ weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService
  function($scope, $resource, cityService) {
 
   $scope.city = cityService.city;
-
+  $scope.weatherResult = {};
   var url= "https://api.openweathermap.org/data/2.5/weather";
   var myKey = config.MY_KEY;
 
 $scope.weatherAPI = $resource(url, {
   callback: "JSON_CALLBACK" }, {get: { method: "JSONP"}});
 
-
-  $scope.weatherResult = $scope.weatherAPI.get({ q:$scope.city, appid: myKey}).$promise.then(function(result){
+var example = $scope.weatherAPI.get({ q:$scope.city, appid: myKey}).$promise.then(function(result){
   console.log(result);
+  $scope.weatherResult = result;
+  console.log("first")
 });
-
+  console.log("second")
   console.log($scope.weatherResult);
+  console.log("example", example);
+  console.log(typeof example);
+
+
 
 }]);
